9a83d54e493f3f6b536f736351d1bbd9678871f0
gTomasik <grzesiek315@gmail.com>
2019-04-16 09:05:26 +0200

    dodany textbox

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Banner from '../Home/Banner';
 import ProjectGrid from './ProjectGrid';
 import Top5Users from '../Home/Top5Users';
+import  Datebox  from './Datebox';
 
 export default class Project extends Component {
     constructor(props) {
@@ -19,7 +20,10 @@ export default class Project extends Component {
       <div>
           {console.log(this.state.img)}
           
-        <div> <img src="img/defaultImage.jpg" height="500" style={imgStyle} alt="Project Image"></img></div>
+        <div style={container}> 
+          <img src="img/defaultImage.jpg" height="500" style={imgStyle} alt="Project Image"></img>
+          <div style ={position}><Datebox/></div>
+        </div>
         <Top5Users/>
         <ProjectGrid/>
         
@@ -37,3 +41,15 @@ const imgStyle = {
     maxHeight: '20%',
 }
 
+const container = {
+  position: 'relative',
+  textAlign: 'center',
+  color: 'white',
+}
+
+const position = {
+  position: 'absolute',
+  top: '30%',
+  left: '45rem',
+  transform: 'translate(-50%, -50%)',
+}
